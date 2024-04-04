fcadb71a91e8ffe339a7e16eeaa509e6e637df3a
CJ <98860750+cbcj3000@users.noreply.github.com>
2024-04-04 14:24:22 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,11 +5,5 @@ from . import views
 urlpatterns = [
     path("PVApp/", include("PVApp.urls")),
     path("admin/", admin.site.urls),
-    # path('map/', views.load_tiff_data, name='map'),
-    path('map/', views.map_view, name='map'),
-    path('map200/', views.connsql, name='map200'),
-    path('Render/', views.render_view, name='Render'),
     path('plotly/', views.plotly_view, name='plotly'),
-    path('testing/', views.testing_view, name='testing'),
-    #path('copy/', views.copy_map, name='copy'),
 ]
